ff2bd158cc0d0e20db191fb162fb49ece282d692
Lukasz <lukasz.zywluk@gmail.com>
2022-06-15 13:28:14 -0400

    Complete code

--- absoluteValueSumMin.py
+++ absoluteValueSumMin.py
@@ -1,2 +1,5 @@
 def absolute_value_sum_min(nums: List[int]) -> int:
-    min_sum = 0
+    listSize = len(nums)
+    isEven = listSize % 2 == 0
+
+    return nums[listSize // 2 - 1] if isEven else nums[listSize // 2]
