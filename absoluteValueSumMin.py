a9e2e23d18c84cb1b04aa6f22e3f4e65cd49f543
Lukasz <lukasz.zywluk@gmail.com>
2022-06-15 14:43:23 -0400

    Add searchSortedRotatedList

--- absoluteValueSumMin.py
+++ absoluteValueSumMin.py
@@ -3,3 +3,27 @@ def absolute_value_sum_min(nums: List[int]) -> int:
     isEven = listSize % 2 == 0
 
     return nums[listSize // 2 - 1] if isEven else nums[listSize // 2]
+
+print(absolute_value_sum_min([2, 4, 7]))
+
+def searchSortedRotatedList(nums: List[int], target: int) -> int:
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = left + (right - left) // 2
+        if nums[mid] == target:
+            return mid
+
+        # left to mid part is sorted
+        if nums[left] <= nums[mid]:
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        else:
+            if nums[mid] < target <= nums[right]:
+                left = mid +1
+            else:
+                right = mid - 1
+    return -1
+
+print(searchSortedRotatedList([0,1,2,5,7,8,9])
